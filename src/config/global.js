b25aa2480d163ada31efaabe479b590c8ac6fd4f
luisEfe80 <luismendez80@hotmail.com>
2021-08-24 19:49:13 -0400

    Adicion de contenido

--- src/config/global.js
+++ src/config/global.js
@@ -5,7 +5,7 @@ export default {
     descripcionCurso:
       'En este componente formativo se abordan los conceptos clave para el diseño y desarrollo de aplicaciones móviles nativas, como sus elementos, vistas, maquetación de entornos de desarrollo, lenguajes etc.',
     imagenBannerPrincipal: require('@/assets/curso/banner-princiapal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/fondo-banner-principal.png'),
+    fondoBannerPrincipal: require('@/assets/curso/fondo-banner-principal.svg'),
   },
   menuPrincipal: {
     menu: [
@@ -96,8 +96,7 @@ export default {
           {
             icono: 'far fa-file-alt',
             numero: '3.1',
-            titulo:
-              'Core data',
+            titulo: 'Core data',
             hash: 't_3_1',
           },
           {
@@ -145,135 +144,95 @@ export default {
   },
   referencias: [
     {
-      referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA: ',
+      referencia: 'Apple. (2021). Apple Books. <i>An all-new chapter</i>',
+      link: 'https://www.apple.com/apple-books',
+    },
+    {
+      referencia: 'Developer. (2021). <i>Enabling cloudkit in your app.</i>',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://developer.apple.com/documentation/cloudkit/enabling_cloudkit_in_your_app',
+    },
+    {
+      referencia: 'Developer. (2021). Soporte para desarrolladores.',
+      link: 'https://developer.apple.com/es/support',
     },
     {
       referencia:
-        'Castellanos, O., Fumeque A., y Ramirez D. (2011). Análisis de tendencias: de la información hacia la innovación. Universidad Nacional de Colombia. Sistema de Bibliotecas SENA: ',
+        'Developer. (2021). <i>SwiftUI essentials creating and combining views.</i>',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
+        'https://developer.apple.com/tutorials/swiftui/creating-and-combining-views',
     },
     {
       referencia:
-        'Harpersbazaar. (2019). La ropa que eliges afecta a tu estado de ánimo y cerebro, según expertos. En arpersbazaar.mx.',
-    },
-    {
-      referencia:
-        'Lafuente M., Navarro J., y Navarro J. (2005). Ilustración de moda. Sistema de Bibliotecas SENA',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000025549',
-    },
-    {
-      referencia:
-        'Laver, J.,  Albizua, H., y Fortea, B. (1990). Breve historia del traje y la moda. Sistema de Bibliotecas SENA. ',
-    },
-    {
-      referencia:
-        'Navarro, J.,  y Lafuente, M. (2010). Ilustración de moda: dibujo plano. Sistema de Bibliotecas SENA:',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000023241',
-    },
-    {
-      referencia:
-        'Áciedad Americana de Ensayos y Materiales (ASTM). (2003). Textiles. Editorial Staff. [Book]. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000051551',
-    },
-    {
-      referencia:
-        'Szkutnicka, B., Cuenca, C., Koyama, A. (2010). El dibujo técnico de moda paso a paso. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+        'Swift - Apple (CO). (2021). Swift. Un poderoso lenguaje al alcance de todos para crear increíbles apps.',
+      link: 'https://www.apple.com/co/swift',
     },
   ],
   glosario: [
     {
-      termino: 'MoodBoard',
+      termino: 'Cocoa',
       significado:
-        'su traducción literal es mapa de inspiración y aunque su uso está muy asociado al ámbito de la decoración de interiores o la moda, <br/>realmente se utiliza en entornos creativos muy diversos, desde el diseño de productos o servicios al audiovisual o el diseño gráfico.',
+        'Es un <i>framework</i> que permite el desarrollo de aplicaciones nativas para macOS.',
     },
     {
-      termino: 'Suftware',
+      termino: 'WWDC',
       significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Pirognóstico',
-      significado:
-        'ensayo sin componentes reactivos, quiere decir, reconocimiento de los componentes, mediante la aplicación adecuada del fuego.',
-    },
-    {
-      termino: 'Áectores',
-      significado:
-        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
-    },
-    {
-      termino: 'Moda',
-      significado:
-        'hábito repetitivo que identifica a una persona, una especie de presión social, que debe consumir, utilizar o hacer.',
-    },
-    {
-      termino: 'Diseño',
-      significado:
-        'actividad creativa orientada a soluciones idóneas a ciertas problemáticas.',
-    },
-    {
-      termino: 'Tipología',
-      significado:
-        'estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
-    },
-    {
-      termino: 'Bidimensional',
-      significado:
-        'que se representa según su altura y su anchura y no su profundidad.',
-    },
-    {
-      termino: 'Interés Inferior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interseccionalidad',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interés superior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+        'Es la Conferencia Mundial de Desarrolladores de Apple, uno de los eventos tecnológicos más importantes del calendario.',
     },
   ],
   complementario: [
     {
       texto:
-        'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
+        'Aplicación <i>Xcode</i>: MoureDev by Brais Moure. (2020). Xcode: cómo crear una app (para principiantes tutorial) [video]. YouTube.',
       tipo: 'Video',
-      link: 'https://www.google.com/',
+      link: 'https://www.youtube.com/watch?v=MyzZnIR5gC4',
     },
     {
       texto:
-        'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+        'Bases de datos cloud: Developer. (2021). <i>Designing and creating a cloudkit database.</i>',
+      tipo: 'Página oficial',
+      link:
+        'https://developer.apple.com/documentation/cloudkit/designing_and_creating_a_cloudkit_database',
+    },
+    {
+      texto: 'Cloudkit: Developer. (2021). Enabling cloudkit in your app. ',
+      tipo: 'Página',
+      link:
+        'https://developer.apple.com/documentation/cloudkit/enabling_cloudkit_in_your_app',
     },
     {
       texto:
-        'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
-      tipo: 'Artículo científico',
-      link: 'https://www.google.com/',
+        'Condicionales: Swift. (2021). Conceptos básicos: el lenguaje de programación Swift 5.5.',
+      tipo: 'Pagina',
+      link: 'https://docs.swift.org/swift-book/LanguageGuide/TheBasics.htm',
     },
     {
-      texto: 'Economía de Rosquilla. (s.f.).',
+      texto: 'Core data: Mastermoviles. (2021). <i>El stack de Core data.</i>',
+      tipo: 'Video',
+      link:
+        'https://mastermoviles.gitbook.io/persistencia-en-dispositivos-m-viles-ios/intro-2/3.2_stack',
+    },
+    {
+      texto:
+        'Core data: MSAFT.Industries. (2018). <i>Programando Core data en iOS [video]. YouTube. </i>',
+      tipo: 'Video',
+      link:
+        'https://mastermoviles.gitbook.io/persistencia-en-dispositivos-m-viles-ios/intro-2/3.2_stack',
+    },
+    {
+      texto: '',
+      tipo: 'Video',
+      link: '',
+    },
+    {
+      texto: '',
+      tipo: 'Video',
+      link: '',
+    },
+    {
+      texto: '',
       tipo: 'Página web',
       descarga: '/downloads/prueba.pdf',
-    },
-    {
-      texto: '2 minutos para entender el desarrollo sostenible – Spanish',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
     },
   ],
   creditos: {
